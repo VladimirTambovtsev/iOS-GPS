7845c3abc0918e56ff15081f52ba0f23eac29d0c
VladimirTambovtsev <tambovcev99@mail.ru>
2020-01-29 16:56:45 +0300

    Initial Commit

---
+++ GPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPS
+//
+//  Created by Vladimir on 29.01.2020.
+//  Copyright © 2020 Vladimir Tambovtsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
